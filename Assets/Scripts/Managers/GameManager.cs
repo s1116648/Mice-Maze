2b34d71c578a65a39a0caeceb3002e56998aae57
Carl <carlzee@live.nl>
2023-06-30 14:51:35 +0200

    StartGameButton does something

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGamePressed()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        UIManager.instance.startLevel();
     }
 }
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager instance;
+
+    [SerializeField]
+    GameObject startScreen;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGamePressed()
+    {
+        GameManager.instance.StartGame();
+    }
+
+    public void startLevel()
+    {
+        startScreen.SetActive(false);
     }
 }
