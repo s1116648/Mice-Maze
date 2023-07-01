635f4818da9c4f7a5b1d60f4a337e80c74b4c82f
Carl <carlzee@live.nl>
2023-07-02 01:13:01 +0200

    cleaned up EnemyController

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -18,26 +18,11 @@ public class EnemyController : MonoBehaviour
 
     void CheckIfSurrounded()
     {
-        bool surrounded = true;
-        if (!DirectionBlocked(Vector3.left)) {
-            surrounded = false;
-        }
-        if (!DirectionBlocked(Vector3.right))
-        {
-           surrounded = false;
-        }
-        if (!DirectionBlocked(Vector3.forward))
-        {
-            surrounded = false;
-        }
-        if (!DirectionBlocked(Vector3.back))
-        {
-            surrounded = false;
-        }
-        if (surrounded)
-        {
-            LevelManager.instance.EnemySurrounded(transform.gameObject);
-        }
+        if (!DirectionBlocked(Vector3.left)) return;
+        if (!DirectionBlocked(Vector3.right)) return;
+        if (!DirectionBlocked(Vector3.forward)) return;
+        if (!DirectionBlocked(Vector3.back)) return;
+        LevelManager.instance.EnemySurrounded(transform.gameObject);
     }
 
     bool DirectionBlocked(Vector3 direction)
@@ -62,26 +47,20 @@ public class EnemyController : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == TagNames.Player)
-        {
             TryFollowPlayer(other.gameObject);
-        }
     }
 
     void TryFollowPlayer(GameObject player)
     {
         if (SeesPlayer(player))
-        {
             Debug.Log("Try to move to player");
-        }
     }
 
     bool SeesPlayer(GameObject player)
     {
         Vector3 direction = FindDirection(player.transform.position);
         if (Physics.Raycast(transform.position, transform.TransformDirection(direction), out RaycastHit hit, 5f))
-        {
             return IsPlayer(hit.transform.gameObject);
-        }
         return false;
     }
 
