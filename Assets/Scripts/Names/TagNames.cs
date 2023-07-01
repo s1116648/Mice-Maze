625eb043b9a50257a1ae6e3bcd3ead2bac393ad4
Carl <carlzee@live.nl>
2023-07-01 12:05:56 +0200

    Food has tag

--- Assets/Scripts/Names/TagNames.cs
+++ Assets/Scripts/Names/TagNames.cs
@@ -6,4 +6,5 @@ public static class TagNames
 {
     public static readonly string Player = "Player";
     public static readonly string Enemy = "Enemy";
+    public static readonly string Food = "Food";
 }
