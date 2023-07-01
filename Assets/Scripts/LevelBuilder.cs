836ff7215ad113e51ea9c3af1e601a4c4d057ebe
Carl <carlzee@live.nl>
2023-07-01 23:03:06 +0200

    Made code easier for upgrades to multiple levels

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -8,7 +8,9 @@ public class LevelBuilder : MonoBehaviour
     public static LevelBuilder instance;
 
     [SerializeField]
-    GameObject playerPrefab, enemyPrefab, cratePrefab, foodPrefab;
+    GameObject
+        playerPrefab, enemyPrefab, cratePrefab, foodPrefab,
+        playersParent, enemiesParent, cratesParent, foodsParent;
 
     readonly string
         level1 = "Assets/LevelData/Level1.txt";
@@ -19,14 +21,18 @@ public class LevelBuilder : MonoBehaviour
     {
         readonly float
             y = 0.5f,
-            startX = -5,
-            startZ = -5;
-        float x, z;
+            startXModifier = -5,
+            startZModifier = 5;
+        float 
+            x, z,
+            startX, startZ;
 
-        public Pointer()
+        public Pointer(float middleX, float middleZ)
         {
-            x = startX;
-            z = startZ;
+            startX = middleX + startXModifier;
+            startZ = middleZ + startZModifier;
+            x = startX + middleX;
+            z = startZ + middleZ;
         }
 
         public void NextPos()
@@ -43,7 +49,7 @@ public class LevelBuilder : MonoBehaviour
         void NewLine()
         {
             x = startX;
-            z++;
+            z--;
         }
 
         public Vector3 Position()
@@ -69,7 +75,7 @@ public class LevelBuilder : MonoBehaviour
 
     public void BuildLevel()
     {
-        pointer = new Pointer();
+        pointer = new Pointer(0, 0); // Later middle of the level.
         string levelData = GetLevelData(level1);
         while (levelData.Length > 0)
         {
@@ -82,28 +88,24 @@ public class LevelBuilder : MonoBehaviour
         char currentThing = levelData[0];
         switch(currentThing)
         {
-            case '#':
-                pointer.NextPos();
+            case '#': // empty
                 break;
             case 'p':
                 SpawnPlayer(pointer.Position());
-                pointer.NextPos();
                 break;
             case 'e':
                 SpawnEnemy(pointer.Position());
-                pointer.NextPos();
                 break;
             case 'c':
                 SpawnCrate(pointer.Position());
-                pointer.NextPos();
                 break;
             case 'f':
                 SpawnFood(pointer.Position());
-                pointer.NextPos();
                 break;
             default:
-                break;
+                return levelData.Substring(1);
         }
+        pointer.NextPos();
         return levelData.Substring(1);
     }
 
@@ -114,21 +116,21 @@ public class LevelBuilder : MonoBehaviour
 
     void SpawnPlayer(Vector3 position)
     {
-        Instantiate(playerPrefab, position, Quaternion.identity);
+        Instantiate(playerPrefab, position, Quaternion.identity, playersParent.transform);
     }
 
     void SpawnEnemy(Vector3 position)
     {
-        Instantiate(enemyPrefab, position, Quaternion.identity);
+        Instantiate(enemyPrefab, position, Quaternion.identity, enemiesParent.transform);
     }
 
     void SpawnCrate(Vector3 position)
     {
-        Instantiate(cratePrefab, position, Quaternion.identity);
+        Instantiate(cratePrefab, position, Quaternion.identity, cratesParent.transform);
     }
 
     void SpawnFood(Vector3 position)
     {
-        Instantiate(foodPrefab, position, Quaternion.identity);
+        Instantiate(foodPrefab, position, Quaternion.identity, foodsParent.transform);
     }
 }
