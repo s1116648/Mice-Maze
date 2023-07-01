2851d03c0a2bdc6e27831a084754c67ee38994c5
Carl <carlzee@live.nl>
2023-07-01 15:47:31 +0200

    Crates can only move when player touches it

---
+++ Assets/Scripts/Objects/CrateController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateController : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == TagNames.Player)
+        {
+            Freeze(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == TagNames.Player)
+        {
+            Freeze(true);
+        }
+    }
+
+    void Freeze(bool frozen)
+    {
+        Rigidbody crate = transform.gameObject.GetComponent<Rigidbody>();
+        if (frozen)
+        {
+            crate.constraints = RigidbodyConstraints.FreezeAll;
+        } else
+        {
+            crate.constraints &= ~RigidbodyConstraints.FreezePositionX;
+            crate.constraints &= ~RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+}
