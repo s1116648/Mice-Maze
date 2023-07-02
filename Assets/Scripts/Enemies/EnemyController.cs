5036db21e91fc0f039ef84ea58eb53fec34e5054
Carl <carlzee@live.nl>
2023-07-02 02:33:05 +0200

    Enemies move forward if posible

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField]
+    GameObject 
+        frontSide,
+        leftSide,
+        rightSide;
+
+    float speed = 3.5f;
+
+    readonly float rayLenght = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +24,7 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
         CheckIfSurrounded();
+        Move();
     }
 
     void CheckIfSurrounded()
@@ -27,14 +38,19 @@ public class EnemyController : MonoBehaviour
 
     bool DirectionBlocked(Vector3 direction)
     {
-        if (Physics.Raycast(transform.position, transform.TransformDirection(direction), out RaycastHit hit, 0.75f))
+        return DirectionBlocked(transform.position, direction);
+    }
+
+    bool DirectionBlocked(Vector3 position, Vector3 direction)
+    {
+        if (Physics.Raycast(position, transform.TransformDirection(direction), out RaycastHit hit, rayLenght))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(direction) * hit.distance, Color.yellow);
+            Debug.DrawRay(position, transform.TransformDirection(direction) * hit.distance, Color.yellow);
             return !IsPlayer(hit.transform.gameObject);
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(direction) * 0.75f, Color.white);
+            Debug.DrawRay(position, transform.TransformDirection(direction) * rayLenght, Color.white);
             return false;
         }
     }
@@ -67,5 +83,51 @@ public class EnemyController : MonoBehaviour
     Vector3 FindDirection(Vector3 destination)
     {
         return destination - transform.position;
+    }
+
+    void Move()
+    {
+        Vector3 forwardDirection = transform.rotation * transform.forward;
+        if (CanMoveForward(forwardDirection))
+        {
+            MoveForward(forwardDirection);
+        }
+    }
+
+    void MoveForward(Vector3 forwardDirection)
+    {
+        float
+            moveX = forwardDirection.x * speed * Time.deltaTime,
+            moveZ = forwardDirection.z * speed * Time.deltaTime;
+        transform.Translate(moveX, 0, moveZ);
+    }
+
+    bool CanMoveForward(Vector3 direction)
+    {
+        if (MiddleForwardBlocked(direction))
+            return false;
+        if (LeftForwardBlocked(direction))
+            return false;
+        if (RightForwardBlocked(direction))
+            return false;
+        return true;
+    }
+
+    bool MiddleForwardBlocked(Vector3 direction)
+    {
+        Vector3 position = frontSide.gameObject.transform.position;
+        return DirectionBlocked(position, direction);
+    }
+
+    bool LeftForwardBlocked(Vector3 direction)
+    {
+        Vector3 position = leftSide.gameObject.transform.position;
+        return DirectionBlocked(position, direction);
+    }
+
+    bool RightForwardBlocked(Vector3 direction)
+    {
+        Vector3 position = rightSide.gameObject.transform.position;
+        return DirectionBlocked(position, direction);
     }
 }
